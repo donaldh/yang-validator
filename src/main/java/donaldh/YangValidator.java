d64c0b6bdf7ffd62932f65f224ef8967ca9f9b3a
Donald Hunter <donaldh@cisco.com>
2016-03-14 18:08:27 +0000

    Use YangTextSchemaContextResolver and resolve root schema manually.

--- src/main/java/donaldh/YangValidator.java
+++ src/main/java/donaldh/YangValidator.java
@@ -1,8 +1,15 @@
 package donaldh;
 
 import java.io.File;
-import java.util.HashSet;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.data.impl.codec.xml.XmlCodecProvider;
@@ -10,70 +17,63 @@ import org.opendaylight.yangtools.yang.data.impl.schema.transform.ToNormalizedNo
 import org.opendaylight.yangtools.yang.data.impl.schema.transform.dom.DomUtils;
 import org.opendaylight.yangtools.yang.data.impl.schema.transform.dom.parser.DomToNormalizedNodeParserFactory;
 import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
-import org.opendaylight.yangtools.yang.model.repo.api.SchemaContextFactory;
-import org.opendaylight.yangtools.yang.model.repo.api.SchemaSourceFilter;
-import org.opendaylight.yangtools.yang.model.repo.api.SchemaSourceRepresentation;
-import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
-import org.opendaylight.yangtools.yang.model.repo.api.YangTextSchemaSource;
-import org.opendaylight.yangtools.yang.model.repo.spi.PotentialSchemaSource;
-import org.opendaylight.yangtools.yang.model.repo.spi.SchemaListenerRegistration;
-import org.opendaylight.yangtools.yang.model.repo.spi.SchemaSourceListener;
-import org.opendaylight.yangtools.yang.model.repo.util.FilesystemSchemaSourceCache;
-import org.opendaylight.yangtools.yang.parser.repo.SharedSchemaRepository;
+import org.opendaylight.yangtools.yang.parser.repo.YangTextSchemaContextResolver;
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 public class YangValidator {
 	
-	private static final File MODELS_FOLDER;
-	
-	static {
-		try {
-			MODELS_FOLDER = new File(YangValidator.class.getResource("/yang/").toURI());
-		} catch (Throwable t) {
-			throw new IllegalStateException(t);
-		}
-	}
-
 	public static void main(String[] args) {
+	    
+	    if (args.length < 2) {
+	        System.err.println("Usage is: YangValidator <schema-dir> <yang-file>");
+	        System.exit(1);
+	    }
+	    
+	    final String schemaDir = args[0];
+	    final String yangfile = args[1];
 		
-		SharedSchemaRepository repo = new SharedSchemaRepository("test");
-		FilesystemSchemaSourceCache<YangTextSchemaSource> fileSourceProvider = 
-				new FilesystemSchemaSourceCache<YangTextSchemaSource>(repo, YangTextSchemaSource.class, MODELS_FOLDER);
-		SchemaContextFactory schemaContextFactory = repo.createSchemaContextFactory(SchemaSourceFilter.ALWAYS_ACCEPT);
-		
-		final Set<SourceIdentifier> allSources = new HashSet<SourceIdentifier>();
-        final SchemaListenerRegistration reg = repo.registerSchemaSourceListener(new SchemaSourceListener() {
+        try {
+            YangTextSchemaContextResolver resolver = YangTextSchemaContextResolver.create("resolver");
+	    
+            for (File f : new File(schemaDir).listFiles()) {
+                resolver.registerSource(f.toURI().toURL());
+            }
+            
+            SchemaContext schema = resolver.getSchemaContext().get();
 
-			public void schemaSourceEncountered(SchemaSourceRepresentation source) {}
+            Map<String, DataSchemaNode> modulesByNodeName = new HashMap<>();
 
-			public void schemaSourceRegistered(Iterable<PotentialSchemaSource<?>> sources) {
-                for (final PotentialSchemaSource<?> source : sources) {
-                	allSources.add(source.getSourceIdentifier());
+            Set<Module> modules = schema.getModules();
+            for (Module m : modules) {
+                for (DataSchemaNode n : m.getChildNodes()) {
+                    modulesByNodeName.put(n.getQName().getLocalName(), n);
                 }
-			}
+            }
 
-			public void schemaSourceUnregistered(PotentialSchemaSource<?> source) {}
-        });
-        reg.close();
-		
-		try {
-			SchemaContext schema = schemaContextFactory.createSchemaContext(allSources).get();
-			
 			XmlCodecProvider codecProvider = DomUtils.defaultValueCodecProvider();
 			DomToNormalizedNodeParserFactory f = DomToNormalizedNodeParserFactory.getInstance(codecProvider, schema);
-			
 			ToNormalizedNodeParser<Element, ContainerNode, ContainerSchemaNode> parser = f.getContainerNodeParser();
 
-			Set<Module> modules = schema.getModules();
-			for (Module m : modules) {
-				m.getDataChildByName(name);
-			}
+			DocumentBuilderFactory namespaceFactory = DocumentBuilderFactory.newInstance();
+            namespaceFactory.setNamespaceAware(true);
+            DocumentBuilder builder = namespaceFactory.newDocumentBuilder();
+            InputStream is = new FileInputStream(new File(yangfile));
+            Document d = builder.parse(is);
+
+            Element root = d.getDocumentElement();
+            String nodeName = root.getLocalName();
+
+            DataSchemaNode schemaNode = modulesByNodeName.get(nodeName);
+            ContainerNode c = parser.parse(Collections.singletonList(root), (ContainerSchemaNode) schemaNode);
+            
+            System.out.println(c);
+
 		} catch (Throwable t) {
 			System.err.println(t.getClass().getName() + " : " + t.getMessage());
 		}
-		
 	}
-
 }
